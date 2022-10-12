ccdf034ec27364c3c7c3df1e54ab921bf6da80c2
takaoS <s.takao0710@gmail.com>
2022-10-12 19:45:16 +0900

    [update] リストページをグリッド表示に対応
    リスト要素 (画像+テキスト) をグリッド表示

--- lib/component_staff_list.dart
+++ lib/component_staff_list.dart
@@ -11,25 +11,40 @@ class _StaffListComponentState extends State<StaffListComponent> {
     return CustomScrollView(
       slivers: [
         SliverGrid(
-          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-            maxCrossAxisExtent: 200.0,
-            mainAxisSpacing: 10.0,
-            crossAxisSpacing: 10.0,
-            childAspectRatio: 4.0,
+          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200.0, //　一つのアイテムの最大横幅
+            mainAxisSpacing: 10.0, // 縦の隙間
+            crossAxisSpacing: 10.0, // 横の隙間
+            childAspectRatio: 0.8, // アスペクト比 (1だと正方形)
+            // https://api.flutter.dev/flutter/rendering/SliverGridDelegateWithMaxCrossAxisExtent-class.html
           ),
           delegate: SliverChildBuilderDelegate(
             (BuildContext context, int index) {
               return Container(
                 alignment: Alignment.center,
-                width: 500.0,
-                height: 500.0,
                 color: Colors.teal[100 * (index % 9)],
-                child: Text('Hello!'),
+                child: SingleChildScrollView(
+                  child: StaffListItem(),
+                ),
               );
             },
             childCount: 20,
           ),
         )
+      ],
+    );
+  }
+}
+
+class StaffListItem extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.network('https://drive.google.com/uc?export=view&id=' +
+            '1ntGLjC5PuoVC59VXP0iOnsGI7M4IIvCp'),
+        Text('Takao Sekiguchi'),
       ],
     );
   }
