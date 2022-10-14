b393945ff62ba08848f10f5fa382ee19ef613d82
takaoS <s.takao0710@gmail.com>
2022-10-14 14:57:52 +0900

    [update] Cloud Firestore からデータ取得
    正しく設定できているか確かめるため、とりあえず取得したデータを表示

--- lib/component_staff_list.dart
+++ lib/component_staff_list.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:terakoya_staff_intro_site/page_staff_detail.dart';
 
@@ -9,42 +10,57 @@ class StaffListComponent extends StatefulWidget {
 class _StaffListComponentState extends State<StaffListComponent> {
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(
-      slivers: [
-        SliverGrid(
-          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-            maxCrossAxisExtent: 200.0,
-            mainAxisSpacing: 10.0,
-            crossAxisSpacing: 10.0,
-            childAspectRatio: 0.8,
-          ),
-          delegate: SliverChildBuilderDelegate(
-            (BuildContext context, int index) {
-              return GestureDetector(
-                onTap: () {
-                  Navigator.of(context)
-                      .push(MaterialPageRoute(builder: (context) {
-                    return StaffDetailPage();
-                  }));
-                },
-                child: Container(
-                  alignment: Alignment.center,
-                  color: Colors.teal[100 * (index % 9)],
-                  child: SingleChildScrollView(
-                    child: StaffListItem(),
-                  ),
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance.collection('staff').snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Text(('something went wrong'));
+        } else if (snapshot.hasData || snapshot.data != null) {
+          return CustomScrollView(
+            slivers: [
+              SliverGrid(
+                gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                  maxCrossAxisExtent: 200.0,
+                  mainAxisSpacing: 10.0,
+                  crossAxisSpacing: 10.0,
+                  childAspectRatio: 0.8,
                 ),
-              );
-            },
-            childCount: 20,
-          ),
-        )
-      ],
+                delegate: SliverChildBuilderDelegate(
+                  (BuildContext context, int index) {
+                    final staff = snapshot.data!.docs[index];
+                    return GestureDetector(
+                      onTap: () {
+                        Navigator.of(context)
+                            .push(MaterialPageRoute(builder: (context) {
+                          return StaffDetailPage();
+                        }));
+                      },
+                      child: Container(
+                        alignment: Alignment.center,
+                        color: Colors.teal[100 * (index % 9)],
+                        child: SingleChildScrollView(
+                          child: StaffListItem(staff),
+                        ),
+                      ),
+                    );
+                  },
+                  childCount: snapshot.data!.docs.length,
+                ),
+              )
+            ],
+          );
+        }
+        return Text("Loading ...");
+      },
     );
   }
 }
 
 class StaffListItem extends StatelessWidget {
+  var staff;
+  StaffListItem(staff) {
+    this.staff = staff;
+  }
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -52,7 +68,7 @@ class StaffListItem extends StatelessWidget {
       children: [
         Image.network('https://drive.google.com/uc?export=view&id=' +
             '1ntGLjC5PuoVC59VXP0iOnsGI7M4IIvCp'),
-        Text('森啓太'),
+        Text(staff['fullName']),
       ],
     );
   }
