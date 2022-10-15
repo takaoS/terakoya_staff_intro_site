1a178aeea68624ca45f1f6ccca385faee00498a7
takaoS <s.takao0710@gmail.com>
2022-10-16 08:02:41 +0900

    [update] 詳細ページに表示項目を追加
    Staff モデル (enum の extention) から情報を引っ張ってこれるか試しつつ、学生スタッフ紹介の詳細ページに表示する項目を増やした。項目はさらに追加予定。

--- lib/model/staff_model.dart
+++ lib/model/staff_model.dart
@@ -8,6 +8,16 @@ enum Staff {
 }
 
 extension StaffExtention on Staff {
+  static final staffColNames = {
+    Staff.SkillTeam: 'スキルチーム',
+    Staff.Terakoya: '寺子屋',
+    Staff.prefectures: '活動する都道府県',
+    Staff.fullName: '名前',
+    Staff.fullName_katakana: 'フリガナ',
+  };
+
+  String get staffColName => staffColNames[this]!;
+
   // enum型 → String型 に変換
   // toString() だけだと Order.Terakoya のようになるので、
   // .split('.').last で Terakoya だけ取り出す
