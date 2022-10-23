d600fe191648378ddcc4d08af26e4946864dad0c
takaoS <s.takao0710@gmail.com>
2022-10-23 23:28:55 +0900

    [clean] 複数ファイルで同じ処理をしていた部分を別ファイルに切り出し
    リストに表示するアイテム (StaffListItem() )を、ソート画面と検索画面の両方で同じ処理をしていたため、別ファイルに切り出した

--- lib/component_staff_list.dart
+++ lib/component_staff_list.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:terakoya_staff_intro_site/component_staff_list_item.dart';
 import 'package:terakoya_staff_intro_site/page_staff_detail.dart';
-import 'package:terakoya_staff_intro_site/model/staff_model.dart';
 
 class StaffListComponent extends StatefulWidget {
   var sort;
@@ -65,35 +65,6 @@ class _StaffListComponentState extends State<StaffListComponent> {
         }
         return Text("Loading ...");
       },
-    );
-  }
-}
-
-class StaffListItem extends StatelessWidget {
-  var staff;
-
-  StaffListItem(staff) {
-    this.staff = staff;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    var info = staff[Staff.prefectures.castToString()];
-
-    if (staff[Staff.Terakoya.castToString()] != '') {
-      info += ' / ' + staff[Staff.Terakoya.castToString()];
-    }
-    if (staff[Staff.SkillTeam.castToString()] != '') {
-      info += ' / ' + staff[Staff.SkillTeam.castToString()];
-    }
-
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Image.network(staff[Staff.imgUrl.castToString()]),
-        Text(staff[Staff.fullName.castToString()]),
-        Text(info),
-      ],
     );
   }
 }
