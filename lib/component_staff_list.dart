242a6a2b67d69fc752345a50a8fc6dda344ad2b2
takaoS <s.takao0710@gmail.com>
2022-10-26 11:56:31 +0900

    [clean] リスト表示部分を別ファイルに切り出し
    ソート画面と検索画面で、同じリスト表示の処理をしていたので、別ファイルに切り出して統合した

---
+++ lib/component_staff_list.dart
@@ -0,0 +1,51 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:terakoya_staff_intro_site/component_staff_item.dart';
+import 'package:terakoya_staff_intro_site/page_staff_detail.dart';
+
+class StaffListComponent extends StatefulWidget {
+  AsyncSnapshot<QuerySnapshot<Object?>> _snapshot;
+  StaffListComponent(this._snapshot);
+
+  @override
+  _StaffListComponentState createState() => _StaffListComponentState();
+}
+
+class _StaffListComponentState extends State<StaffListComponent> {
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      slivers: [
+        SliverGrid(
+          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200.0,
+            mainAxisSpacing: 10.0,
+            crossAxisSpacing: 10.0,
+            childAspectRatio: 0.8,
+          ),
+          delegate: SliverChildBuilderDelegate(
+            (BuildContext context, int index) {
+              final _staff = widget._snapshot.data!.docs[index];
+              return GestureDetector(
+                onTap: () {
+                  Navigator.of(context)
+                      .push(MaterialPageRoute(builder: (context) {
+                    return StaffDetailPage(_staff);
+                  }));
+                },
+                child: Container(
+                  alignment: Alignment.center,
+                  color: Colors.teal[100 * (index % 9)],
+                  child: SingleChildScrollView(
+                    child: StaffListItem(_staff),
+                  ),
+                ),
+              );
+            },
+            childCount: widget._snapshot.data!.docs.length,
+          ),
+        )
+      ],
+    );
+  }
+}
--- lib/component_staff_search.dart
+++ lib/component_staff_search.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:terakoya_staff_intro_site/component_staff_item.dart';
-import 'package:terakoya_staff_intro_site/page_staff_detail.dart';
+import 'package:terakoya_staff_intro_site/component_staff_list.dart';
 import 'package:terakoya_staff_intro_site/model/staff_model.dart';
 
 class StaffSearchComponent extends StatefulWidget {
@@ -28,39 +27,7 @@ class _StaffSearchComponentState extends State<StaffSearchComponent> {
         if (snapshot.hasError) {
           return const Text('不具合が発生しました…サポートロボにお問い合わせください (error code: 002)');
         } else if (snapshot.hasData || snapshot.data != null) {
-          return CustomScrollView(
-            slivers: [
-              SliverGrid(
-                gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                  maxCrossAxisExtent: 200.0,
-                  mainAxisSpacing: 10.0,
-                  crossAxisSpacing: 10.0,
-                  childAspectRatio: 0.8,
-                ),
-                delegate: SliverChildBuilderDelegate(
-                  (BuildContext context, int index) {
-                    final _staff = snapshot.data!.docs[index];
-                    return GestureDetector(
-                      onTap: () {
-                        Navigator.of(context)
-                            .push(MaterialPageRoute(builder: (context) {
-                          return StaffDetailPage(_staff);
-                        }));
-                      },
-                      child: Container(
-                        alignment: Alignment.center,
-                        color: Colors.teal[100 * (index % 9)],
-                        child: SingleChildScrollView(
-                          child: StaffListItem(_staff),
-                        ),
-                      ),
-                    );
-                  },
-                  childCount: snapshot.data!.docs.length,
-                ),
-              )
-            ],
-          );
+          return StaffListComponent(snapshot);
         }
         return const CircularProgressIndicator();
       },
